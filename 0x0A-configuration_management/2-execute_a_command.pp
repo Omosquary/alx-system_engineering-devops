8a46366983919397b418dfeafe4e1a84898ce6df
Omotayo Olamilekan Omotosho <iamomosquare@gmail.com>
2022-10-17 09:00:28 +0100

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill process killmenow 
-  
- exec { 'pkill': 
-   command  => 'pkill killmenow', 
-   provider => 'shell', 
- }
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
